49a94dd1fc85d96100d5783a98d9c530434f3e3b
c22quiambao <christelquiambao@gmail.com>
2023-11-22 09:06:37 -0800

    updated

--- client.js
+++ client.js
@@ -23,6 +23,7 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Successfully connected to the game");
     conn.write("Name: EVQ");
+    setupInput ();
   });
 
   // event listeners for when connection is closed
@@ -31,6 +32,27 @@ const connect = function() {
   });
 
   return conn;
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  };
 };
 
 module.exports = connect;
