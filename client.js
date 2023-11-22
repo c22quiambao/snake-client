1dfeffd0bb7752343c6768aceb97e046dda491b5
c22quiambao <christelquiambao@gmail.com>
2023-11-22 07:47:51 -0800

    updated

---
+++ client.js
@@ -0,0 +1,35 @@
+// function implementation
+// establishes a connection with the game server
+const net = require("net");
+
+
+const connect = function() {
+  // create connection
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // event listener for incoming data
+  conn.on("data", (data) => {
+    console.log("Server says:", data);
+  });
+
+  // event listeners for when connection is successful
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game");
+    conn.write("Name: EVQ");
+  });
+
+  // event listeners for when connection is closed
+  conn.on("close", () => {
+    console.log("Connection terminated");
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,32 +1,8 @@
-const net = require("net");
+// Importing required functions
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
+// Attempt to connect
+connect();
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
-  // event listener for incoming data
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-const connection = connect();
-
-// additional event listeners can be added if needed
-connection.on("connect", () => {
-  console.log("Successfully connected to the game");
-});
-
-connection.on("close", () => {
-  console.log("Connection terminated");
-});
 
